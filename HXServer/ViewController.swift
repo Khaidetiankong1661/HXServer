94d63d1301bfbe2e3fe1ab32f6fe0e377c2e7757
wanghaixi <wanghaixi@hongbaodai.com>
2018-01-11 14:19:39 +0800

    Initial Commit

---
+++ HXServer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HXServer
+//
+//  Created by hongbaodai on 2018/1/11.
+//  Copyright © 2018年 wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
